fa60fd4dfb5712abdfcd6d92bb37b869e6500001
sxixa <akasyaajibrilian@gmail.com>
2026-09-10 10:25:04 +0700

    more

--- api/index.php
+++ api/index.php
@@ -1,4 +1,10 @@
 <?php
 
-// Forward requests to Laravel's public entrypoint
+// Create ephemeral SQLite file in Vercel's writable directory
+$dbFile = '/tmp/database.sqlite';
+if (!file_exists($dbFile)) {
+    touch($dbFile);
+}
+
+// Forward requests to Laravel's standard entrypoint
 require __DIR__ . '/../public/index.php';
