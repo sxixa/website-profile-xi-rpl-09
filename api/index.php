86fcd83f4aa7c6d3377b2bcbe33ec779b313054b
sxixa <akasyaajibrilian@gmail.com>
2026-09-21 13:51:07 +0700

    Configure sqlite temp storage for Vercel deployment

--- api/index.php
+++ api/index.php
@@ -1,5 +1,6 @@
 <?php
 
+// 1. Create temporary writable directories
 $dirs = [
     '/tmp/storage/app',
     '/tmp/storage/framework/cache/data',
@@ -15,7 +16,14 @@ foreach ($dirs as $dir) {
     }
 }
 
+// 2. Create blank SQLite database file for database-less mode
+if (!file_exists('/tmp/database.sqlite')) {
+    touch('/tmp/database.sqlite');
+}
+
+// 3. Set storage path
 putenv('LARAVEL_STORAGE_PATH=/tmp/storage');
 $_ENV['LARAVEL_STORAGE_PATH'] = '/tmp/storage';
 
+// 4. Forward request to Laravel public entrypoint
 require __DIR__ . '/../public/index.php';
